cf849a877daf1e87c62fa95af0d0b208e339fa75
Arctic Ice Studio <development@arcticicestudio.com>
2018-11-17 13:58:10 +0100

    Initial implementation of the ESLint config and ignore pattern
    The `.eslintrc.js` configuration file has been placed in the project
    root next to the `.eslintignore` file that defines the ignore pattern.
    It specifies the environments (1) which define global variables that
    are predefined:
    
    - `browser` - browser global variables.
    - `node` - Node.js global variables and Node.js scoping.
    
    References:
    
      (1) https://eslint.org/docs/user-guide/configuring#specifying-environments
    
    GH-15

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2018-present Arctic Ice Studio <development@arcticicestudio.com>
+ * Copyright (C) 2018-present Sven Greb <development@svengreb.de>
+ *
+ * Project:    Nord Docs
+ * Repository: https://github.com/arcticicestudio/nord-docs
+ * License:    MIT
+ */
+
+/**
+ * @file The ESLint configuration.
+ * @author Arctic Ice Studio <development@arcticicestudio.com>
+ * @author Sven Greb <development@svengreb.de>
+ * @see https://github.com/babel/eslint-plugin-babel#rules
+ * @see https://github.com/tc39/proposal-optional-chaining
+ */
+
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  }
+};
