687d30f2d5cdde5b757506cede39d48b92d6d9aa
Arctic Ice Studio <development@arcticicestudio.com>
2018-12-03 11:50:04 +0100

    Create "theme mode" constants for Nord "auras"
    Initially Nord Docs will be build with the "bright snow flurry" and
    "dark night frost" theme modes representing Nord "auras". The names
    have been implemented as constants to be used with the theme utility
    functions to define the styles of a component for each available global
    theme.
    
    GH-53

---
+++ src/styles/theme/constants.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2018-present Arctic Ice Studio <development@arcticicestudio.com>
+ * Copyright (C) 2018-present Sven Greb <development@svengreb.de>
+ *
+ * Project:    Nord Docs
+ * Repository: https://github.com/arcticicestudio/nord-docs
+ * License:    MIT
+ */
+
+/**
+ * @file Provides theme constants.
+ * @author Arctic Ice Studio <development@arcticicestudio.com>
+ * @author Sven Greb <development@svengreb.de>
+ * @since 0.2.0
+ */
+
+/**
+ * Name of Nord's "bright snow flurry" aura for usage as theme mode.
+ *
+ * @constant {string}
+ * @since 0.2.0
+ */
+const MODE_BRIGHT_SNOW_FLURRY = "BRIGHT_SNOW_FLURRY";
+
+/**
+ * Name of Nord's "dark night frost" aura for usage as theme mode.
+ *
+ * @constant {string}
+ * @since 0.2.0
+ */
+const MODE_DARK_NIGHT_FROST = "DARK_NIGHT_FROST";
+
+/**
+ * The name of the theme key for "theme modes".
+ *
+ * @constant {string}
+ * @since 0.2.0
+ */
+const THEME_KEY_MODE = "mode";
+
+export { MODE_BRIGHT_SNOW_FLURRY, MODE_DARK_NIGHT_FROST, THEME_KEY_MODE };
--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -17,8 +17,14 @@
 import globals from "./globals";
 import normalize from "./normalize";
 import typography from "./typography";
+import { MODE_BRIGHT_SNOW_FLURRY, MODE_DARK_NIGHT_FROST, THEME_KEY_MODE } from "./constants";
 
-const theme = { typography };
+const theme = {
+  typography,
+  MODE_BRIGHT_SNOW_FLURRY,
+  MODE_DARK_NIGHT_FROST,
+  THEME_KEY_MODE
+};
 
 export { globals, normalize, typography };
 
