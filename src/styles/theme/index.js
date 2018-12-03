c0e5f6f764fac04e8555df690096d0492eda9535
Arctic Ice Studio <development@arcticicestudio.com>
2018-12-03 13:39:06 +0100

    Implement basic configurations for motion related styles
    This commit implements some basic configurations for motion related
    styles like animations inspired by "Material Designs" (1) great
    documentations and guidelines about motion speed (2). This includes
    values for speed like transition durations. It'll be used to set the
    CSS transition for the base background and font color within the
    `globals` CSS style module.
    
    References:
      (1) https://material.io
      (2) https://material.io/design/motion/speed.html
    
    GH-53

--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -16,16 +16,18 @@
 
 import colors, { nord, palettes } from "./colors";
 import globals from "./globals";
+import motion from "./motion";
 import normalize from "./normalize";
 import { themedMode, themedModeVariant } from "./utils";
 import typography from "./typography";
 import { MODE_BRIGHT_SNOW_FLURRY, MODE_DARK_NIGHT_FROST, THEME_KEY_MODE } from "./constants";
 
-const theme = { colors, typography };
+const theme = { colors, motion, typography };
 
 export {
   colors,
   globals,
+  motion,
   nord,
   normalize,
   palettes,
