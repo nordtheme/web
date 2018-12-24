7cf4e0434edf6c1c26e9f252730ea8b54ae00e3c
Arctic Ice Studio <development@arcticicestudio.com>
2018-12-24 09:52:36 +0100

    Implement core atom component `Link`
    Implemented the core atom `Link` that wraps the base HTML element atom
    component the `A` (1) and adds matching brand styles to it.
    
    References:
      (1) https://github.com/arcticicestudio/nord-docs/issues/70
    
    Associated epic: GH-63
    GH-105

--- test/components/atoms/core/HTMLElements/inlineTextSemantics/A.test.jsx
+++ test/components/atoms/core/HTMLElements/inlineTextSemantics/A.test.jsx
@@ -17,21 +17,25 @@ import { metadataNordDocs } from "data/project";
 describe("logical behavior", () => {
   test("renders inernal URLs with `to` prop", () => {
     const { container } = render(<A to={ROUTE_DOCS}>Docs</A>);
+    expect(container.firstChild).toHaveAttribute("href", ROUTE_DOCS);
     expect(container.firstChild).toMatchSnapshot();
   });
 
   test("renders inernal URLs with `href` prop", () => {
     const { container } = render(<A href={ROUTE_DOCS}>Docs</A>);
+    expect(container.firstChild).toHaveAttribute("href", ROUTE_DOCS);
     expect(container.firstChild).toMatchSnapshot();
   });
 
   test("renders external URLs with `href` prop", () => {
     const { container } = render(<A href={metadataNordDocs.homepage}>Docs</A>);
+    expect(container.firstChild).toHaveAttribute("href", metadataNordDocs.homepage);
     expect(container.firstChild).toMatchSnapshot();
   });
 
   test("renders external URLs with `to` prop", () => {
     const { container } = render(<A to={metadataNordDocs.homepage}>Docs</A>);
+    expect(container.firstChild).toHaveAttribute("href", metadataNordDocs.homepage);
     expect(container.firstChild).toMatchSnapshot();
   });
 });
