b6243eb3449a36f073fb3494dc4f53751e983d41
Arctic Ice Studio <development@arcticicestudio.com>
2019-01-26 14:52:59 +0100

    Implement new SVG vector section divider components
    They render a "swaying" and "wobbly" wave vector graphics used for the
    community page sections.
    
    GH-115

---
+++ src/components/atoms/core/vectors/divider/WaveSwaying.jsx
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2018-present Arctic Ice Studio <development@arcticicestudio.com>
+ * Copyright (C) 2018-present Sven Greb <development@svengreb.de>
+ *
+ * Project:    Nord Docs
+ * Repository: https://github.com/arcticicestudio/nord-docs
+ * License:    MIT
+ */
+
+import React from "react";
+
+import DividerSvg from "./DividerSvg";
+
+/**
+ * A SVG vector graphic divider component rendering a "swaying" wave form.
+ *
+ * @author Arctic Ice Studio <development@arcticicestudio.com>
+ * @author Sven Greb <development@svengreb.de>
+ * @since 0.7.0
+ * @see https://developer.mozilla.org/de/docs/Web/SVG
+ */
+const WaveSwaying = props => (
+  <DividerSvg {...props} viewBox="0 0 1440 94" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 .91c37.102-3.789 72.529 8.207 112.12 35.083 104.792 71.133 186.932 18.896 299.41-18.297 112.476-37.193 128.544 64.109 218.349 43.87 89.805-20.238 155.519-61.91 327.519-21.16s253.141 66.825 357.309 36.95c49.177-14.104 85.113-37.259 125.293-57.88V94H0V.91z" />
+  </DividerSvg>
+);
+
+export default WaveSwaying;
