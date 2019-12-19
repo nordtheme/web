fbfa2a6e4dc2a0796f0167bb69b7505843e29d34
Arctic Ice Studio <development@arcticicestudio.com>
2019-12-19 11:53:23 +0100

    Prepare release version 0.22.0

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,7 +26,7 @@ import {
  *
  * @author Arctic Ice Studio <development@arcticicestudio.com>
  * @author Sven Greb <development@svengreb.de>
- * @version 0.21.0
+ * @version 0.22.0
  * @since 0.1.0
  */
 const Landing = ({ location: { pathname } }) => (
